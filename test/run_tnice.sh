2d0554761a658a1befb644187b29cc05aa5a09b5
Diego F. (NC) <diegoefe@gmail.com>
2018-08-23 15:56:33 -0300

    Script to enable glib debug

---
+++ test/run_tnice.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Stop on assertions:
+# export G_DEBUG=fatal_warnings
+export G_DEBUG=fatal_criticals
+
+# Logging from all glib based libraries
+export G_MESSAGES_DEBUG=all
+
+# Check memory allocation 
+export G_SLICE=debug-blocks
+
+./tnice "$@"
+
