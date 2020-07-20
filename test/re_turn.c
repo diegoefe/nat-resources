a28305433e0772625d8afd46de7d6000e6c72a2c
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2020-07-20 16:55:50 -0300

    turn config

--- test/re_turn.c
+++ test/re_turn.c
@@ -1,3 +1,8 @@
+#include "turn_cfg.h"
+#include <stdint.h>
+#include <re.h>
+
+
 int main() {
 	return 0;
 }
---
+++ test/sample-turn_cfg.h
@@ -0,0 +1,8 @@
+#ifndef TURN_CFG_H_INCLUIDO
+#define TURN_CFG_H_INCLUIDO
+
+#define MY_TURN_HOST "turn.sarasa.com:3478"
+#define MY_TURN_USER "pepe"
+#define MY_TURN_PASS "sanchez"
+
+#endif
