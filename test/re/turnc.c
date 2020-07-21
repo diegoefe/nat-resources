0c29afe83f8bee9e9a5b1c2201d4f40d31a61538
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2020-07-21 16:53:31 -0300

    Building turn* demo

--- test/re/turnc.c
+++ test/re/turnc.c
@@ -6,11 +6,46 @@
 #include <re.h>
 
 int main() {
+	struct dnsc *dnsc = NULL;
+	int maxfds = 4096;
 	int err = libre_init();
 	if(err) {
 		re_fprintf(stderr, "re init failed: %s\n", strerror(err));
 		goto out;
 	}
+
+	enum poll_method method = poll_method_best();
+	switch (method) {
+		case METHOD_SELECT:
+			maxfds = 1024;
+			break;
+		default:
+			maxfds = 32768;
+			break;
+	}
+	err = fd_setsize(maxfds);
+	if (err) {
+		re_fprintf(stderr, "cannot set maxfds to %d: %m\n",
+			   maxfds, err);
+		goto out;
+	}
+
+	err = poll_method_set(method);
+	if (err) {
+		re_fprintf(stderr, "could not set polling method '%s' (%m)\n",
+			   poll_method_name(method), err);
+		goto out;
+	}
+
+	re_printf("using async polling method '%s' with maxfds=%d\n",
+		  poll_method_name(method), maxfds);
+
+	err = dns_init(&dnsc);
+	if (err) {
+		(void)re_fprintf(stderr, "dnsinit: %m\n", err);
+		goto out;
+	}
+
 #if 0
 	struct turnc* sTC;
 
@@ -23,6 +58,7 @@ int main() {
 
  out:
 	// mem_deref(sTC);
+	mem_deref(dnsc);
 	libre_close();
 
 	mem_debug();
---
+++ test/re/util.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <re.h>
+#include <re.h>
+
+int dns_init(struct dnsc **dnsc)
+{
+	struct sa nsv[8];
+	uint32_t nsn;
+	int err;
+
+	nsn = ARRAY_SIZE(nsv);
+
+	err = dns_srv_get(NULL, 0, nsv, &nsn);
+	if (err) {
+		(void)re_fprintf(stderr, "dns_srv_get: %m\n", err);
+		goto out;
+	}
+
+	err = dnsc_alloc(dnsc, NULL, nsv, nsn);
+	if (err) {
+		(void)re_fprintf(stderr, "dnsc_alloc: %m\n", err);
+		goto out;
+	}
+
+ out:
+	return err;
+}
