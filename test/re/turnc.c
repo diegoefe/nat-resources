992d1b0f1142543732087869ae1972e064e287b5
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2020-07-21 16:15:17 -0300

    re to its own dir

--- test/re/turnc.c
+++ test/re/turnc.c
@@ -7,13 +7,22 @@
 
 int main() {
 	int err = libre_init();
-	if (err) {
+	if(err) {
 		re_fprintf(stderr, "re init failed: %s\n", strerror(err));
 		goto out;
 	}
+#if 0
+	struct turnc* sTC;
+
+	err = turnc_alloc(&sTC, const struct stun_conf *conf, int proto,
+		void *sock, int layer, const struct sa *srv,
+		MY_TURN_USER, MY_TURN_PASS,
+		uint32_t lifetime, turnc_h *th, void *arg);
+
+#endif
 
  out:
-	//mem_deref(sdp);
+	// mem_deref(sTC);
 	libre_close();
 
 	mem_debug();
