d300678401fb8a1cc3bc9916f4ca68c09a9f65aa
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2020-07-21 15:21:22 -0300

    More structure for turn client

--- test/re_turn.c
+++ test/re_turn.c
@@ -1,14 +1,22 @@
 #include "turn_cfg.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <stdint.h>
 #include <re.h>
 
-
 int main() {
-	libre_init();
+	int err = libre_init();
+	if (err) {
+		re_fprintf(stderr, "re init failed: %s\n", strerror(err));
+		goto out;
+	}
 
-
+ out:
+	//mem_deref(sdp);
 	libre_close();
-	return 0;
+
+	mem_debug();
+	tmr_debug();
+	return err;
 }
