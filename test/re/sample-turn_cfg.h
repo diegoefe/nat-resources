fc45333af9ec6884ec787e8201ec44028a4f75ca
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2020-07-22 14:29:19 -0300

    crc32 link fix (z)

--- test/re/sample-turn_cfg.h
+++ test/re/sample-turn_cfg.h
@@ -1,7 +1,10 @@
 #ifndef TURN_CFG_H_INCLUIDO
 #define TURN_CFG_H_INCLUIDO
 
-#define MY_TURN_HOST "turn.sarasa.com:3478"
+#define MY_TURN_HOST "turn.sarasa.com"
+#define MY_TURN_PORT "3478"
+#define MY_TURN_URL MY_TURN_HOST ":" MY_TURN_PORT
+
 #define MY_TURN_USER "pepe"
 #define MY_TURN_PASS "sanchez"
 
